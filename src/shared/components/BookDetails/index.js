c0e8aff47d2cbfd33b8450baae2f7311a07d0140
oliqwe <oliqwe@gmail.com>
2021-03-25 07:55:10 +0200

    fix children rendering

--- src/shared/components/BookDetails/index.js
+++ src/shared/components/BookDetails/index.js
@@ -1,34 +1,64 @@
-import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Rating from '@material-ui/lab/Rating'
-import { number, string } from 'prop-types'
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core'
+import { useAsync } from 'shared/hooks/useAsync'
+import { GOOGLE_BOOKS_API_URL } from 'shared/constants/constants'
+import BookInfo from './BookInfo'
+import Loading from 'shared/components/Loading'
+import BookDetailsHeader from './BookDetailsHear'
+import Box from '@material-ui/core/Box'
+import { renderAllBut, renderComponent } from './utils'
 
-function BookDetailsHeader({ title, subtitle, averageRating }) {
+const useStyles = makeStyles(theme => ({
+  bookDetailsContainer: {
+    margin: theme.spacing(2),
+    position: 'relative',
+  },
+}))
+
+function Actions({ children, ...props }) {
+  return <Box my={1}>{React.cloneElement(children, { ...props })}</Box>
+}
+
+function BookDetails({ bookId, children }) {
+  const { data, get, status } = useAsync()
+  const bookInfo = data?.volumeInfo
+  const classes = useStyles()
+  const arrayChildren = React.Children.toArray(children)
+
+  useEffect(() => {
+    if (bookId) {
+      get(`${GOOGLE_BOOKS_API_URL}volumes/${bookId}`)
+    }
+  }, [get, bookId])
+
   return (
-    <Grid container spacing={2}>
-      <Grid item container justify="space-between">
-        <Grid item xs={10}>
-          <Typography variant="h5" gutterBottom>
-            {title}
-          </Typography>
-          <Typography variant="body2" gutterBottom>
-            {subtitle}
-          </Typography>
-        </Grid>
-        <Grid item xs={2}>
-          <Typography variant="caption">Google Rating</Typography>
-          <Rating name="google-rating" value={averageRating || 0} readOnly />
-        </Grid>
-      </Grid>
-    </Grid>
+    <div className={classes.bookDetailsContainer}>
+      {status === 'pending' && <Loading />}
+      {bookInfo ? (
+        <>
+          <BookDetailsHeader
+            title={bookInfo.title}
+            subtitle={bookInfo.subtitle}
+            averageRating={bookInfo.averageRating}
+          />
+          {arrayChildren.map(child =>
+            typeof child.type === 'string'
+              ? child
+              : React.cloneElement(child, { bookId, bookInfo, ...child.props }),
+          )}
+          <BookInfo info={bookInfo} />
+        </>
+      ) : null}
+    </div>
   )
 }
 
-BookDetailsHeader.propTypes = {
-  title: string,
-  subtitle: string,
-  averageRating: number,
+BookDetails.Actions = Actions
+
+BookDetails.propTypes = {
+  bookId: PropTypes.string,
+  children: PropTypes.any,
 }
 
-export default BookDetailsHeader
+export default BookDetails
